8095fd392d939f8deb52ae270657497ba3eee297
shivu1919 <shivamthakur.ss129@gmail.com>
2024-12-26 18:56:44 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="main">
+      <h1>Tic Tac Toe</h1>
+        <div className="board">
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+        </div>
+    </div>
+  )
+}
+
+export default App
